ffe04c6d565b92bc8b71a0a35f78ed6fe587943d
Xing Wu <41498567+silverlobster@users.noreply.github.com>
2019-09-03 14:51:16 -0400

    Initial commit

---
+++ app/src/main/java/com/example/xingy/snake/SnakeActivity.java
@@ -0,0 +1,48 @@
+package com.example.xingy.snake;
+
+import android.app.Activity;
+import android.graphics.Point;
+import android.os.Bundle;
+import android.view.Display;
+
+
+public class SnakeActivity extends Activity {
+    //Declare an instance of SnakeEngine
+    //We will code this soon
+    SnakeEngine snakeEngine;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Get the pixel dimensions of the screen
+        Display display = getWindowManager().getDefaultDisplay();
+
+        //Initialize the result into a Point object
+        Point size = new Point();
+        display.getSize(size);
+
+        //Create a new instance of the snakeEngine class
+        snakeEngine = new SnakeEngine(this, size);
+
+        //Make snakeEngine the view of the Activity
+        setContentView(snakeEngine);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        snakeEngine.resume();
+    }
+
+    //Stop the thread in snakeEngine
+    @Override
+    protected void onPause() {
+        super.onPause();
+        snakeEngine.pause();
+    }
+}
+
+
+
